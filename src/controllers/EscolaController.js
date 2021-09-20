d19719807274ecf8523988242984995db3bceb25
Jefferson Neves <jefferson.rpn@gmail.com>
2021-09-20 15:07:55 -0300

    Consulta escolas por nome, cidade e UF

--- src/controllers/EscolaController.js
+++ src/controllers/EscolaController.js
@@ -1,10 +1,31 @@
+const { Op } = require('sequelize');
 const Escola = require('../models/Escola');
 
 module.exports = {
   async search(req, res) {
     const { nome, cidade, uf, ensino } = req.query;
-    console.log(uf);
-    const escola = await Escola.findOne();
+
+    const whereClause = [];
+    if (typeof nome !== 'undefined') {
+      whereClause.push({
+        'no_entidade': { [Op.like]: '%' + nome + '%' }
+      });
+    }
+    if (typeof cidade !== 'undefined') {
+      whereClause.push({ 'co_municipio': cidade });
+    }
+    if (typeof uf !== 'undefined') {
+      whereClause.push({ 'co_uf': uf });
+    }
+
+    const escola = await Escola.findAll({
+      where: {
+        [Op.and]: [
+          whereClause
+        ]
+      }
+    });
+
     return res.json(escola);
   }
 }
