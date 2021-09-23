6ece6c987f638ff3f1fcac4da041e6ac07cc3842
Jefferson Neves <jefferson.rpn@gmail.com>
2021-09-23 10:50:46 -0300

    Remove log

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,6 @@
 const Sequelize = require('sequelize');
 const config = require('../config/database');
 
-console.log(config);
-
 const Escola = require('../models/Escola');
 
 const connection = new Sequelize(config);
