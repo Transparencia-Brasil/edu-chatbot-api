86e3602a9beb053f668a653576883318d4ae3690
Jefferson Neves <jefferson.rpn@gmail.com>
2021-10-12 15:36:24 -0300

    Adiciona endpoints para avaliação de cada fase

--- src/controllers/AvaliacaoController.js
+++ src/controllers/AvaliacaoController.js
@@ -1,7 +1,7 @@
 const { Op, Sequelize } = require('sequelize');
 const Resposta = require('../models/Resposta');
 
-const { calcularPontuacaoTotal } = require('../scripts/avaliacao');
+const { calcularPontuacaoTotal, calcularPontuacaoFaseAulas, calcularPontuacaoFaseInfra } = require('../scripts/avaliacao');
 
 module.exports = {
   async get(req, res) {
@@ -28,6 +28,62 @@ module.exports = {
     res.json({
       pontuacao: avaliacao.pontuacao,
       resultado: avaliacao.resultado,
+      resposta
+    });
+  },
+
+  async getAulas(req, res) {
+    const user_id = req.params.user_id
+    const resposta = await Resposta.findOne({
+      where: {
+        user_id: user_id
+      },
+      order: [
+        ['created_at', 'DESC']
+      ]
+    });
+
+    if (!resposta) {
+      res.json({
+        pontuacao: null,
+        resultado: "Resposta não encontrada",
+        resposta: null
+      });
+    }
+
+    const pontuacao = calcularPontuacaoFaseAulas(resposta);
+
+    res.json({
+      pontuacao: pontuacao,
+      resultado: "--",
+      resposta
+    });
+  },
+
+  async getInfra(req, res) {
+    const user_id = req.params.user_id
+    const resposta = await Resposta.findOne({
+      where: {
+        user_id: user_id
+      },
+      order: [
+        ['created_at', 'DESC']
+      ]
+    });
+
+    if (!resposta) {
+      res.json({
+        pontuacao: null,
+        resultado: "Resposta não encontrada",
+        resposta: null
+      });
+    }
+
+    const pontuacao = calcularPontuacaoFaseInfra(resposta)
+
+    res.json({
+      pontuacao: pontuacao,
+      resultado: "--",
       resposta
     });
   }
