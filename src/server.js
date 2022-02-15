7f08b7f41e88e98ef695e5152fbd5b9c087c419d
Jefferson Neves <jefferson.rpn@gmail.com>
2022-02-14 22:57:55 -0300

    Adicina limite de acesso à API

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
+const rateLimit = require("express-rate-limit");
 
-require('./database');
+require("./database");
 
 const app = express();
+
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000,
+    max: 500,
+    standardHeaders: true,
+    legacyHeaders: false,
+  })
+);
 
 app.use(express.json());
 app.use(routes);
